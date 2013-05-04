94ff202469f9cb6cbc6b0a757e9a1cf1be736fd4
Jérémy Schoffen <jschoffen.dev@gmail.com>
2013-05-04 16:00:22 +0200

    Rules all converted in clojure data.

--- src/generator/lesserdark/theme.clj
+++ src/generator/lesserdark/theme.clj
@@ -3,69 +3,86 @@
         clojure.repl)
   (:import java.io.File))
 
+(def rules vector)
+(def group-rules concat)
 
 (def theme
   (rules
-   "/*
-http://lesscss.org/ dark theme
-Ported to CodeMirror by Peter Kroon
-*/"
+   "/*"
+   "  http://lesscss.org/ dark theme"
+   "  Ported to CodeMirror by Peter Kroon"
+   "*/"
 
-[:#multi.dark2  :background :#262626 ]
+   [:#multi.dark2  :background :#262626 ]
+   [[:div.CodeMirror :.cm-s-dark2
+     :span.CodeMirror-matchingbracket]
+    :color :#7EFC7E]
 
-[:.cm-s-dark2 :line-height :1.3em]
+   ""
+   ""
+   "/*Inside parent theme*/"
+   ""
+   [:.cm-s-dark2
 
-[:.cm-s-dark2
- :background :#262626
- :color :#EBEFE7
- :text-shadow [0 :-1px :1px :#262626]]
+    :line-height :1.3em
+    :background :#262626
+    :color :#EBEFE7
+    :text-shadow [0 :-1px :1px :#262626]
 
- [[:.cm-s-dark2 :div.CodeMirror-selected]
-  :background: [:#45443B :!important]] "/* 33322B*/"
+    [:div.CodeMirror-selected
+     :background: [:#45443B :!important]]
 
-[[:.cm-s-dark2 :.CodeMirror-cursor]
- :border-left: [:1px :solid :white :!important]]
-   "/*editable code holder*/"
-[[:.cm-s-dark2 :.CodeMirror-lines]
- :margin-left :3px
- :margin-right :3px]
+    [:.CodeMirror-cursor
+     :border-left: [:1px :solid :white :!important]]
 
-   "/*65FC65*/"
-[[:div.CodeMirror :.cm-s-dark2
-  :span.CodeMirror-matchingbracket]
- :color :#7EFC7E]
+    "/*editable code holder*/"
 
-[[:.cm-s-dark2 :.CodeMirror-gutter]
- :background :#262626
- :border-right [:1px :solid :#aaa]
- :padding-right :3px
- :min-width :2.5em ]
+    [:.CodeMirror-lines
+     :margin-left :3px
+     :margin-right :3px]
 
-[[:.cm-s-dark2 :.CodeMirror-gutter-text] :color :#777]
+    [:.CodeMirror-gutter
+     :background :#262626
+     :border-right [:1px :solid :#aaa]
+     :padding-right :3px
+     :min-width :2.5em ]
 
-[[:.cm-s-dark2 :span.cm-keyword]    :color :#599eff]
-[[:.cm-s-dark2 :span.cm-atom]       :color :#C2B470]
-[[:.cm-s-dark2 :span.cm-number]     :color :#B35E4D]
-[[:.cm-s-dark2 :span.cm-def]        :color :white]
-[[:.cm-s-dark2 :span.cm-variable]   :color :#D9BF8C]
-[[:.cm-s-dark2 :span.cm-variable-2] :color :#669199]
-[[:.cm-s-dark2 :span.cm-variable-3] :color :white]
-[[:.cm-s-dark2 :span.cm-property]   :color :#92A75C]
-[[:.cm-s-dark2 :span.cm-operator]   :color :#92A75C]
-[[:.cm-s-dark2 :span.cm-comment]    :color :#666]
-[[:.cm-s-dark2 :span.cm-string]     :color :#BCD279]
-[[:.cm-s-dark2 :span.cm-string-2]   :color :#f50]
-[[:.cm-s-dark2 :span.cm-meta]       :color :#738C73]
-[[:.cm-s-dark2 :span.cm-error]      :color :#9d1e15]
-[[:.cm-s-dark2 :span.cm-qualifier]  :color :#555]
-[[:.cm-s-dark2 :span.cm-builtin]    :color :#ff9e59]
-[[:.cm-s-dark2 :span.cm-bracket]    :color :#EBEFE7]
-[[:.cm-s-dark2 :span.cm-tag]        :color :#669199]
-[[:.cm-s-dark2 :span.cm-attribute]  :color :#00c]
-[[:.cm-s-dark2 :span.cm-header]     :color :#a0a]
-[[:.cm-s-dark2 :span.cm-quote]      :color :#090]
-[[:.cm-s-dark2 :span.cm-hr]         :color :#999]
-[[:.cm-s-dark2 :span.cm-link]       :color :#00c]
+    [:.CodeMirror-gutter-text :color :#777]
+    [:span.cm-keyword    :color :#599eff]
+    [:span.cm-atom       :color :#C2B470]
+    [:span.cm-number     :color :#B35E4D]
+    [:span.cm-def        :color :white]
+    [:span.cm-variable   :color :#D9BF8C]
+    [:span.cm-variable-2 :color :#669199]
+    [:span.cm-variable-3 :color :white]
+
+    [:span.cm-property   :color :#92A75C]
+    [:span.cm-operator   :color :#92A75C]
+    [:span.cm-comment    :color :#666]
+    [:span.cm-string     :color :#BCD279]
+
+
+    [:span.cm-string-2   :color :#f50]
+    [:span.cm-meta       :color :#738C73]
+    [:span.cm-error      :color :#9d1e15]
+    [:span.cm-qualifier  :color :#555]
+    [:span.cm-builtin    :color :#ff9e59]
+
+    [:span.cm-bracket    :color :#EBEFE7]
+    [:span.cm-tag        :color :#669199]
+    [:span.cm-attribute  :color :#00c]
+    [:span.cm-header     :color :#a0a]
+    [:span.cm-quote      :color :#090]
+    [:span.cm-hr         :color :#999]
+    [:span.cm-link       :color :#00c]
+
+    "/*I'm here*/"
+    ]
+
+
+
+
+
 ))
 
 
